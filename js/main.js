57ff92690b021335e1d9b93008b1c36a49378f47
DOUARA Abderrahman <Abderrahman@DOUARAs-MacBook-Air-2.local>
2018-06-17 15:20:18 +0100

    viewModel functionality

--- js/main.js
+++ js/main.js
@@ -1,22 +1,96 @@
 // DOM Objects 
-let $mainInputs = $('#main-inputs');
+let $mainInputs = $('#main-inputs-0');
 let	$formContainer = $('#form-container');
 let $addNewRowButton = $('#addNewRowButton');
 
-// function to appending new row 
-let addNewRow = function() {
-	$formContainer.append('<div class="row" id="main-inputs">' + $mainInputs.html() + "</div>");
+// Row's Index Tracking Variable
+let rowIndex = 1; 
+
+// Function to append new row 
+let addNewRow = function(index) {
+	$formContainer.append('<div class="row" id="main-inputs-' + index + '">' + 
+		                  $mainInputs.html() + "</div>");
 }
 
-// Adding 3 rows by default 
-for(let i=0; i<3; i++){
-	addNewRow();
+// Adding 3 rows on pageload
+for(let i=1; i<4; i++){
+	addNewRow(i);
+	rowIndex = i; // Increasing Row Index by 1 with each iteration
 }
 
-// Add new row when click the button
+// On click button add new row
 $addNewRowButton.click(function() {
-	addNewRow();
+	addNewRow(rowIndex + 1);
+	// Bind the new row with new instance of the singleRowViewModel
+	viewModelInstance = new singleRowViewModel;
+	ko.applyBindings(viewModelInstance, 
+		             document.getElementById('main-inputs-'+(rowIndex + 1)));
+	
+	// push the row's result to the finalResultViewModelInstance
+	finalResultViewModelInstance.observablearray.push(viewModelInstance.rowResult);
+    
+    // Increasing Row Index by 1
+    rowIndex = rowIndex + 1;
 });
 
-// Knockoutjs ViewModel 
-// Colecting values from the inputs 
+
+
+// Knockoutjs ViewModels
+// ViewModel for final result
+let finalResultViewModel = function() {
+	  let self = this;
+	  // An observable array to hold each row's result 
+	  self.observablearray = ko.observableArray();
+	  
+	  // An array to hold each row's result after value change
+	  let lastChangedValues = [];
+	  
+	  // function to caculate the sum of array values
+	  lastChangedValues.sum = function () {
+	  	    let result = 0;
+	  	    for(let i=0; i < lastChangedValues.length; i++){
+        	   result = result + lastChangedValues[i]
+        	}
+        	return result;
+      }
+      /* Pushing the last results after value change to lastChangedValues array
+		And returning the result
+      */
+      self.finalResult = ko.computed(function() {
+      	    for(let i=0; i < self.observablearray().length; i++){
+        	   lastChangedValues[i] = self.observablearray()[i]();
+        	}
+        	return  lastChangedValues.sum();
+        		
+    	}, self);
+};
+let finalResultViewModelInstance = new finalResultViewModel;
+ko.applyBindings(finalResultViewModelInstance, document.getElementById('result'));
+
+// ViewModel to calculate the result of each row
+let singleRowViewModel = function() {
+	let self = this;
+	    self.quantity =  ko.observable(0);        
+        self.capacity =  ko.observable(0);
+        self.dailyOperatingHours =  ko.observable(0);
+
+        this.rowResult = ko.computed(function() {
+        	return (self.quantity() * self.capacity() * self.dailyOperatingHours());
+    	}, self);
+}
+// Binding new instance for each row
+for(let i=0; i <= rowIndex; i++){
+  viewModelInstance = new singleRowViewModel;
+  ko.applyBindings(viewModelInstance, document.getElementById('main-inputs-'+i));
+  finalResultViewModelInstance.observablearray.push(viewModelInstance.rowResult);
+};
+
+
+
+
+
+
+
+
+
+
